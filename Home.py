dd14b80f50bba073079376404c12cc63e6c311cd
kelvesc <kelves_16@hotmail.com>
2024-08-13 19:07:19 -0300

    (chore): removed data profiling and tweaked Home page.

--- Home.py
+++ Home.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title = "PISI3 - BSI - UFRPE por Gabriel Alves",
+    page_title = "Playlists automáticas",
     layout = "wide",
     menu_items = {
-        'About': '''Este sistema foi desenvolvido pelo prof Gabriel Alves para fins didáticos, para a disciplina de 
+        'About': '''Este sistema foi desenvolvido originalmente pelo prof Gabriel Alves para fins didáticos, para a disciplina de 
         Projeto Interdisciplinar para Sistemas de Informação 3 (PISI3) do 3° período do curso de Bacharelado em Sistemas de Informação
         (BSI) da Universidade Federal Rural de Pernambuco (UFRPE).
         Dúvidas? gabriel.alves@ufrpe.br
@@ -14,7 +14,7 @@ st.set_page_config(
 )
 
 st.markdown(f'''
-    <h1>Sistema Didático para PISI3</h1>
+    <h1>Crição automática de plylists</h1>
     <br>
     Este projeto tem o objetivo de prover vários exemplos úteis para os projetos que serão desenvolvidos na disicplina de 
     Projeto Interdisciplinar para Sistemas de Informação 3 (PISI3) do 3° período do curso de Bacharelado em Sistemas de Informação
@@ -30,6 +30,10 @@ st.markdown(f'''
             <li>Aprendizado de Máquina: Agrupamento e Classificação.</li>
     </ul>
     Classroom: <a href="https://classroom.google.com/c/NjExNTAzOTU4MDQy?cjc=7qgaz7u">https://classroom.google.com/c/NjExNTAzOTU4MDQy?cjc=7qgaz7u</a><br>
-    Contato: gabriel.alves@ufrpe.br<br>
+    Contato: 
+            <li> gabriel.alves@ufrpe.br</li>
+            <li> gabriel.augustos@ufrpe.br</li>
+            <li> kelves.nunes@ufrpe.br</li>
+            <li> kelves.nunes@ufrpe.br</li>
     Acesse: <a href="bsi.ufrpe.br">bsi.ufrpe.br</a>
 ''', unsafe_allow_html=True)
